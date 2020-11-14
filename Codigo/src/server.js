2760175d056d22fb038d5e49563d7f420f8837ff
Victor Campos <victorboaventcampos@gmail.com>
2020-11-14 14:09:18 -0300

    Atualização da master
    Master atualizada conforme as branchs

---
+++ Codigo/src/server.js
@@ -0,0 +1,35 @@
+const proffys = [
+    { name: "Vinícius Marini", avatar: "https://avatars0.githubusercontent.com/u/57442852?v=4", whatsapp: "31999887744", bio: "Limpeza de qualidade e muita atenção. Sou profissional de limpeza há dez anos e já atendi mais de 2.000 casas!", subject: "Limpeza Completa", cost: "R$89.999", weekday: [0], time_from: [720], time_for: [1220] },
+
+    { name: "Victor teste", avatar: "https://avatars1.githubusercontent.com/u/59877368?s=400&u=7ac037f2fa198a5abcea41cfaf46ed404c046f64&v=4", whatsapp: "31999887744", bio: "Limpeza de qualidade e muita atenção. Sou profissional de limpeza há dez anos e já atendi mais de 2.000 casas!", subject: "Limpeza de Apartamentos", cost: "R$89.999", weekday: [2], time_from: [700], time_for: [1220] },
+
+    { name: "Arthur teste", avatar: "https://avatars1.githubusercontent.com/u/55153181?s=460&u=ee933c48a1851d7c6d59bc99d0a68ee019d10b95&v=4", whatsapp: "31999887744", bio: "Limpeza de qualidade e muita atenção. Sou profissional de limpeza há dez anos e já atendi mais de 2.000 casas!", subject: "Limpeza de Apartamentos", cost: "R$89.999", weekday: [2], time_from: [700], time_for: [1220] }
+]
+
+function pageLanding(req, res) {
+    return res.render("index.html")
+}
+
+function pageFeedCliente(req, res) {
+    return res.render("feedCliente.html", { proffys })
+}
+
+function pageCadastro(req, res) {
+    return res.render("Cadastro.html")
+}
+const express = require('express')
+const server = express()
+
+//nunjucks
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views/', {
+    express: server,
+    noCache: true,
+})
+
+//rotas da aplicação
+server.use(express.static("public"))
+    .get("/", pageLanding)
+    .get("/feedCliente", pageFeedCliente)
+    .get("/Cadastro", pageCadastro)
+    .listen(5500)
