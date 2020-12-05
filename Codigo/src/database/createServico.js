3ecfd12ba6eadc849a96d102862e52a0abfaf844
Victor Campos <victorboaventcampos@gmail.com>
2020-12-04 21:55:40 -0300

    Atualização

---
+++ Codigo/src/database/createServico.js
@@ -0,0 +1,38 @@
+module.exports = async function(db, { disponibilidadeValor, classScheduleValues }) {
+
+
+    //INSERIR DADOS NA TABELA ENDERECO
+
+    const insertedDisponibilidade = await db.run(`
+    INSERT INTO DISPONIBILIDADE (
+        valor,
+        descricao,
+        diarista_id
+    ) VALUES (
+        "${disponibilidadeValor.valor}",
+        "${disponibilidadeValor.bio}",
+        "${diarista_id}"
+    );
+    `)
+    const disponibilidade_id = insertedDisponibilidade.lastID;
+
+    const insertedAllClassScheduleValues = classScheduleValues.map((classScheduleValue) => {
+        return db.run(`
+           INSERT INTO HORARIOS (
+             dia_semana,
+             tempo_de,
+             tempo_ate,
+             disponibilidade_id
+           ) VALUES (
+            "${classScheduleValue.weekday}",
+            "${classScheduleValue.time_from}",
+            "${classScheduleValue.time_to}",
+            "${disponibilidade_id}"
+           );   
+        `)
+    })
+
+    //AQUI VOU EXECUTAR TODOS OS DB.RUNS() DAS CLASS_SCHEDULES
+    await Promise.all(insertedAllClassScheduleValues)
+
+}
