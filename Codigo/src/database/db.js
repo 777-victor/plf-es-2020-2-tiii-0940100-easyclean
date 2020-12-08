7d2c8836e8c688b6978c7d497024691114eef312
Vinícius Marini <57442852+marinisz@users.noreply.github.com>
2020-12-08 19:36:19 -0300

    Revert "Revert "Merge branch 'master' of https://github.com/icei-pucminas/plf-es-2020-2-tiii-0940100-easyclean""
    This reverts commit 8636bbf47bca828cd97c3a23329a2d9ccdba18cf.

--- Codigo/src/database/db.js
+++ Codigo/src/database/db.js
@@ -12,6 +12,7 @@ function execute(db) {
       NOME TEXT, 
       DATA_CADASTRO datetime default current_timestamp,
       EMAIL TEXT,
+      SENHA TEXT,
       IMG_DIARISTA TEXT,
       TELEFONE TEXT,
       LOGRADOURO_ID INTEGER
@@ -44,6 +45,7 @@ function execute(db) {
       CPF TEXT,
       NOME TEXT,
       EMAIL TEXT,
+      SENHA TEXT,
       IMG_CLIENTE TEXT,
       TELEFONE TEXT,
       LOGRADOURO_ID INTEGER
