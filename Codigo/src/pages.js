2a4a9ad0dc6efd612cce09513b28e4c9ab84fa25
Vinícius Marini <57442852+marinisz@users.noreply.github.com>
2020-12-11 12:23:19 -0300

    UP

--- Codigo/src/pages.js
+++ Codigo/src/pages.js
@@ -50,7 +50,7 @@ async function pageIndicadores(req, res) {
         //console.log(indica3)
         console.log(indica4)
         console.log(indica5)
-        return res.render("indicadores.html", { indica1 ,indica2 , indica4 })
+        return res.render("indicadores.html", { indica1 ,indica2 ,indica3, indica4,indica5 })
     } catch (error) {
         console.log(error)
     }
